2afea9e332459047f732a92ce76e6c1125a28f48
F0rtr4n-C0d3 <82851047+F0rtr4n-C0d3@users.noreply.github.com>
2021-05-16 11:54:08 -0300

    Repositorio actualizado

--- ejercicios_practica/ejercicio_3.py
+++ ejercicios_practica/ejercicio_3.py
@@ -10,7 +10,7 @@
 # Ejercicios de práctica numérica
 
 # Condicionales anidados
-numero_1 = 7
+numero_1 = 5
 numero_2 = -2
 
 # Verifique si el numero_1 es mayor a 5
@@ -22,6 +22,32 @@ numero_2 = -2
 #      verifique si el numero_2 es mayor a 5
 #       --> En caso afirmativo imprima en pantalla "Resp=3"
 #       --> En caso negativo imprima en pantalla "Resp=4"
+
+
+#Inicio de actividad :
+
+print('Bienvenido a la actividad xD')
+
+if numero_1 > 5:
+    print('El primer número si es mayor')
+    if numero_2 > 0:
+        print('resp=1')
+    elif numero_2 < 0:
+        print('resp=2')
+
+elif numero_1 < 5:
+    print('El primer número es menor a 5')
+    if numero_2 > 5:
+        print('Resp=3')
+    elif numero_2 < 5:
+        print('Resp=4')
+
+else:
+    if numero_1 == 5:
+        print('El primer numero es igual a 5')
+
+print('Fin de la actividad')
+
 
 # Verifique la calificación de un estudiante según su
 # puntaje en un examen
@@ -36,11 +62,17 @@ puntaje = 70
 # Debe imprimir en pantalla la calificacion
 # Utilizar "if" anidados
 
+#Inicio de actividad 2 
 
-#Inicio de actividad :
+if puntaje >= 90:
+    print('A')
+elif puntaje >= 80:
+    print('B')
+elif puntaje >= 70:
+    print('C')        
+elif puntaje >= 60:
+    print('D')    
+elif puntaje < 60:
+    print('F')
 
-print('Bienvenido a mi actividad')
-
-if 
-
-    
+print('Fin de actividad 2')
--- ejercicios_practica/ejercicio_4.py
+++ ejercicios_practica/ejercicio_4.py
@@ -9,8 +9,8 @@
 
 # Ejemplos variables de texto
 
-texto_1 = '5'
-texto_2 = '7'
+texto_1 = 'cinco'
+texto_2 = 'dos'
 
 # Verifique cual cual de los dos textos es mayor alfabéticamente
 # Imprima en pantalla según corresponda
@@ -19,6 +19,32 @@ texto_2 = '7'
 # en nuevas variables númericas (int)
 # Repita el proceso, ¿Cuál de las nuevas variables es mayor?
 # Imprima en pantalla según corresponda
+
+palabra_1 = len(texto_1)
+palabra_2 = len(texto_2)
+
+if texto_1 > texto_2:
+    print('Texto 1 es mayor a Texto 2')
+elif texto_1 < texto_2:
+    print('Texto 2 Es mayor a texto 1')
+
+if palabra_1 > palabra_2:
+    print('Palabra 1 es mayor a palabra 2')
+elif palabra_2 > palabra_1:
+    print('Palabra 2 es mayor a Palabra 1')
+
+#fin de actividad
+
+
+
+
+
+
+
+
+
+
+
 
 # Para pensar!
 # ¿Por qué cree que texto_2 es mayor a texto_1?
